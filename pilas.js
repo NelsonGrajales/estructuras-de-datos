81f8c30dafeb440e5332f011c013a1513817f4e6
NelsonGrajales <ngrajales2332@gmail.com>
2024-05-13 21:39:59 -0500

    pila

--- pilas.js
+++ pilas.js
@@ -1,7 +1,26 @@
-let pila = [1, 2]
-function pilas(pila) {
-    let tope = pila[pila.length - 1]
-    console.log(tope)
+class Pila {
+    constructor() {
+        this.pila = [];
+        this.length = this.pila.length;
+        this.cima = this.pila.length - 1;
+    }
+
+    push(dato) {
+        this.cima++
+        this.pila[this.cima] = dato;
+        console.log(this.pila)
+    }
+
+    pull() {
+        this.pila[this.cima] = 0
+        console.log(this.pila)
+    }
 }
 
-pilas()
+let pila = new Pila
+pila.push(1)
+pila.push(2)
+pila.push(22)
+pila.push(23)
+pila.push(24)
+pila.pull()
